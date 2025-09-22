71b1b915380887fd8296455928bb2ce9b7f8490e
Nefi Zaldana <nefizaldana@gmail.com>
2025-09-21 21:16:39 -0600

    w3-activities

--- server.js
+++ server.js
@@ -1,34 +1,68 @@
 /* ******************************************
  * This server.js file is the primary file of the
  * application. It is used to control the project.
- *******************************************/
+ ***************************************** */
 /* ***********************
  * Require Statements
- *************************/
+ *********************** */
+const inventoryRoute = require("./routes/inventoryRoute");
+const baseController = require("./controllers/baseController");
+const utilities = require("./utilities/");
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
 const app = express();
+app.use(express.static("public"));
 const static = require("./routes/static");
 
 /* ***********************
  * View Engine and Templates
- *************************/
-
+ * ********************** */
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // not at views root
 
-// Index route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" });
-});
 /* ***********************
-* Routes
-
-
-/**************************/
+ * Routes
+ * ********************** */
 app.use(static);
+//app.use(require("./routes/static"));
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome));
+// app.get("/", function (req, res) {
+//   res.render("index", { title: "Home" });
+// });
+// Inventory routes
+//app.use("/inv", inventoryRoute);
+// Inventory routes
+app.use("/inv", inventoryRoute);
+
+/* *********************
+ * File Not Found Route - must be last route in list
+ * Place after all routes
+ * ********************** */
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+/* **************************
+ * Express Error Handler
+ * Place after all other middleware
+ * ************************ */
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  });
+});
 
 /* ***********************
  * Local Server Information
